f06fc5b4496ec78fe4ef23b7b72dae83dc3021ce
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2022-10-25 22:06:17 +0900

    second

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello, <a href="https://nextjs.org">Next.js!</a>
+          A Psalm of Life <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
